4e604b2f64939e2f6276cd91d9dd07889c9a76ea
Matt Newton <newton10471@gmail.com>
2012-04-19 22:57:10 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppRails321::Application.config.secret_token = '71965624f1ae66e58c5c65413c77a247e84d225f5d7a2cb5c8a4dc0e3817ada64359547ec096cbbd6abdebc6c97cb63016335a250368f3978e6009ebe9728d7a'
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,159 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "index" do
+    
+    let(:user) { FactoryGirl.create(:user) }
+
+    before do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+
+    describe "pagination" do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      let(:first_page)  { User.paginate(page: 1) }
+      let(:second_page) { User.paginate(page: 2) }
+
+      it { should have_link('Next') }
+      it { should have_link('2') }
+      it { should_not have_link('delete') }
+
+      it "should list the first page of users" do
+        first_page.each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+
+      it "should not list the second page of users" do
+        second_page.each do |user|
+          page.should_not have_selector('li', text: user.name)
+        end
+      end
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+
+      it "should list each user" do
+        User.all[0..2].each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
+  describe "signup page" do 
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button "Sign up" }.not_to change(User, :count)
+      end
+    end
+
+    describe "error messages" do
+      before { click_button "Sign up" }
+
+      let(:error) { 'errors prohibited this user from being saved' }
+
+      it { should have_selector('title', text: 'Sign up') }
+      it { should have_content(error) }
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      describe "after saving the user" do
+        before { click_button "Sign up" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.flash.success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
+      it "should create a user" do
+        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      end
+    end
+
+    describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        sign_in user
+        visit edit_user_path(user)
+      end
+
+      describe "visiting the edit page" do
+        it { should have_selector('h1',    text: "Edit user") }
+        it { should have_selector('title', text: "Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      end
+
+      describe "with invalid information" do
+        let(:error) { '1 error prohibited this user from being saved' }
+        before { click_button "Update" }
+
+        it { should have_content(error) }
+      end
+
+      describe "with valid information" do
+        let(:user)      { FactoryGirl.create(:user) }
+        let(:new_name)  { "New Name" }
+        let(:new_email) { "new@example.com" }
+        before do
+          fill_in "Name",         with: new_name
+          fill_in "Email",        with: new_email
+          fill_in "Password",     with: user.password
+          fill_in "Confirmation", with: user.password
+          click_button "Update"
+        end
+      
+        it { should have_selector('title', text: new_name) }
+        it { should have_selector('div.flash.success') }
+        it { should have_link('Sign out', href: signout_path) }
+        specify { user.reload.name.should  == new_name }
+        specify { user.reload.email.should == new_email }
+      end
+    end
+  end
+end
